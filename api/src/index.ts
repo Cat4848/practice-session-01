3b9c2d57b7571eacf817051c719b1e7a5389edbf
Cat4848 <catalinrol@gmail.com>
2024-01-27 16:13:19 +0000

    feat/put-user-api-endpoint-completed

--- api/src/index.ts
+++ api/src/index.ts
@@ -18,4 +18,8 @@ app.get("/users/edit/:id", (req, res) => {
   res.send("Here is the edit particular user form.");
 });
 
+app.put("/users/:id", (req, res) => {
+  res.send("User edited in the database.");
+});
+
 app.listen(4000);
