d92c7c210ccbbbc2b4c6de1ec2ee3b778f746ebd
Cat4848 <catalinrol@gmail.com>
2024-01-27 18:30:09 +0000

    feat:dashboard-router-added

--- client/src/components/DashboardRoot.tsx
+++ client/src/components/DashboardRoot.tsx
@@ -1,3 +1,20 @@
+import { Outlet, Link } from "react-router-dom";
+
 export default function DashboardRoot() {
-  return <h1>Dashboard Layout</h1>;
+  return (
+    <>
+      <h1>Dashboard Layout</h1>;
+      <nav>
+        <ul>
+          <li>
+            <Link to="dashboard/accounts">Accounts</Link>
+          </li>
+          <li>
+            <Link to="dashboard/reports">Reports</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
 }
