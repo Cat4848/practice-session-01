22dc5d692aee16c660ab2438187e919f52f89405
Cat4848 <catalinrol@gmail.com>
2024-01-27 17:34:19 +0000

    feat:navbar-outline

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home Page</div>
+  }
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
     <App />
   </React.StrictMode>
 );
